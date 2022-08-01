1cf29c96aac0c88a852e72e0f9b703e6356f5924
Egor Ivanov <egexiii@yandex-team.ru>
2022-08-01 21:05:20 +0300

    -

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,8 +1,10 @@
 describe('Test', () => {
     it('simple test with node ' + process.version, async () => {
-        await browser.url(`https://hd.kinopoisk.ru`);
+        browser.url(`https://hd.kinopoisk.ru`);
         const btnSelector = '[class*=ProfileMenu__title]';
-        await expect($(btnSelector)).toBeDisplayedInViewport();
+        expect($(btnSelector)).toBeDisplayedInViewport();
+        expect($(btnSelector)).toBeDisplayedInViewport();
+        expect($(btnSelector)).toBeDisplayedInViewport();
         await browser.pause(2000);
         await expect($(btnSelector)).toBeDisplayedInViewport();
     });
